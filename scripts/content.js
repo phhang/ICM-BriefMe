41d151e601d981f664d9bf08415b3579cd2f7d7c
Xianhang He <xianhanghe@microsoft.com>
2023-02-12 22:22:07 -0800

    V1 extension

---
+++ scripts/content.js
@@ -0,0 +1,66 @@
+const debugModeFlag = true;
+const tableFoldThresholdRows = 5;
+const tableFoldThresholdColumns = 6;
+
+const loadTime = performance.now();
+window.addEventListener('load', foldLargeTable);
+
+function foldTableElement(divElement) {
+    if (divElement === null) {
+        return false;
+    }
+    const commentTable = divElement.querySelector("table");
+    if (commentTable === null) {
+        return false;
+    }
+    const tableRows = commentTable.rows.length;
+    const tableColumns = commentTable.rows[0].cells.length;
+    // if (debugModeFlag) {
+    //     console.log(`tableRows: ${tableRows}, tableColumns: ${tableColumns}`);
+    // }
+    if (tableRows < tableFoldThresholdRows && tableColumns < tableFoldThresholdColumns) {
+        return; // Only fold large tables
+    }
+    const tableSize = `Table size: ${tableRows} x ${tableColumns}`;
+    const detailsElement = document.createElement("details");
+    const detailsSummary = document.createElement("summary");
+    detailsSummary.textContent = tableSize;
+    detailsElement.appendChild(detailsSummary);
+    detailsElement.appendChild(commentTable);
+    divElement.appendChild(detailsElement);
+    return true;
+}
+
+function foldLargeTable(){
+    const startTime = performance.now();
+    const commentDivBody = document.querySelectorAll("div.body");
+    let foldCount = 0;
+    for (var i = 0; i < commentDivBody.length; i++) {
+        var divBody = commentDivBody[i];
+        // TODO: Check case where: div.body span (div) table
+        if (foldTableElement(divBody)) {
+            foldCount++;
+        }
+        const spanElements = divBody.querySelectorAll("span");
+        for (var j = 0; j < spanElements.length; j++) {
+            var spanElement = spanElements[j];
+            if (foldTableElement(spanElement)) {
+                foldCount++;
+            }
+            const divElements = spanElement.querySelectorAll("div");
+            for (var k = 0; k < divElements.length; k++) {
+                var divElement = divElements[k];
+                if (foldTableElement(divElement)) {
+                    foldCount++;
+                }
+            }
+        }
+    }
+    const endTime = performance.now();
+
+    if (debugModeFlag) {
+        console.log(`foldCount: ${foldCount}`);
+        console.log(`Extension Time taken: ${(endTime - startTime).toFixed(1)} ms`);
+        console.log(`ICM time taken: ${(startTime - loadTime).toFixed(1)} ms`);
+    }
+}
